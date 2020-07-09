c93469cb753b48874fb95c059b6a5b13aa987e9f
Alek97 <a.borisov@blubito.com>
2020-07-10 01:50:12 +0300

    add unit testing

---
+++ tests/App.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+describe("App.test.js", () => {
+
+  beforeEach(() => {
+  });
+
+  it('equals messages to ["Cat"]', () => {
+    expect(true).toBeTruthy();
+  });
+});
